77af3f1481c59dd4e41419e090b6d4ecf9a7310b
vikaskoli9960 <123530791+vikaskoli9960@users.noreply.github.com>
2023-05-29 13:19:04 +0530

    first commit

---
+++ message.js
@@ -0,0 +1,10 @@
+let doNotshere = 'this variable is nt shere'
+
+const message = () => {
+    const name = " akash ";
+    const age = 40;
+    return name + ' is ' +
+        age + ' years old.';
+};
+
+export default message;
